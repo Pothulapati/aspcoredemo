3b62f08c8a14dd41f4073481ab4290bf46c61c63
tarun <tarun@DESKTOP-44E10V7>
2016-07-16 22:44:19 +0530

    up

--- myaspdemo/src/myaspdemo/Controllers/ValuesController.cs
+++ myaspdemo/src/myaspdemo/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace myaspdemo.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "values";
         }
 
         // POST api/values
